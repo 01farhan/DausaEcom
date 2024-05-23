a5ab9e203fb3f58b20d4a857bd70427267cde77d
farhan <arfarhan0545@gmail.com>
2024-05-23 14:40:11 +0530

    dausa

--- src/Contact.js
+++ src/Contact.js
@@ -41,7 +41,7 @@ const Contact = () => {
       <h2 className="common-heading">Contact page</h2>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.265588856342!2d73.91455641541671!3d18.562061287384868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20-%20Viman%20Nagar!5e0!3m2!1sen!2sin!4v1664345115285!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56932.97460117362!2d76.30176428723242!3d26.893626099485807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396d8cc629f972ed%3A0xfb13d0ff4d9ec652!2sDausa%2C%20Rajasthan%20303303!5e0!3m2!1sen!2sin!4v1716455331172!5m2!1sen!2sin"
         width="100%"
         height="400"
         style={{ border: 0 }}
